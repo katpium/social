f2497144843652e45b59e4489042658a14fdc6e8
katpium <uwuing88@gmail.com>
2026-05-19 09:53:42 +0000

    yes

--- myspace-clone/app/page.tsx
+++ myspace-clone/app/page.tsx
@@ -5,48 +5,17 @@ import ProfileBox from "./components/Profilebox";
 import BlogEntry from "./components/BlogEntry";
 import AboutBox from "./components/AboutBox";
 
-
-
-
 export default function Home() {
   return (
     <main>
 
       <header />
-      <Navbar />
+      <NavBar />
 
         <main className="container">
 
           <section className="section-left">
-
-          <h2>John</h2>
-
-          <p>[edit photo]</p>
-
-          <img
-            src="/profile.png"
-            alt="profile"
-          />
-
-          <p>Living!</p>
-
-          <p>[edit]</p>
-
-          <h2>ONLINE!</h2>
-
-          <p>
-            <strong>Mood:</strong> Content [edit]
-          </p>
-
-          <p>
-            <strong>View my:</strong>
-
-            <a href="#blog"> Blog </a>
-
-            <a href="#pic"> Pics </a>
-
-            <a href="#video"> Videos </a>
-          </p>
+            <ProfileSideBar />
 
           </section>
 
