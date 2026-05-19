1276ff3ba3e10c8622696efb632ec64149b13518
katpium <uwuing88@gmail.com>
2026-05-19 16:27:22 +0700

    yes

--- myspace-clone/app/page.tsx
+++ myspace-clone/app/page.tsx
@@ -1,65 +1,36 @@
 import header from "./components/header"
 import NavBar from "./components/NavBar";
+import ProfileSideBar from "./components/ProfileSideBar"
 import ProfileBox from "./components/Profilebox";
 import BlogEntry from "./components/BlogEntry";
 import AboutBox from "./components/AboutBox";
 
-
-
-
 export default function Home() {
   return (
-    <main>
+      <main>
 
-      <header />
-      <NavBar />
+        <header />
+        <NavBar />
 
-      <main className="container">
+        <main className="container">
 
-        <section className="section-left">
+          <section className="section-left">
 
-          <h2>John</h2>
+            <ProfileSideBar />
 
-          <p>[edit photo]</p>
+          </section>
 
-          <img
-            src="/profile.png"
-            alt="profile"
-          />
+          <section className="section-right">
 
-          <p>Living!</p>
+            <ProfileBox />
 
-          <p>[edit]</p>
+            <BlogEntry />
 
-          <h2>ONLINE!</h2>
+            <AboutBox />
+          </section>
 
-          <p>
-            <strong>Mood:</strong> Content [edit]
-          </p>
-
-          <p>
-            <strong>View my:</strong>
-
-            <a href="#blog"> Blog </a>
-
-            <a href="#pic"> Pics </a>
-
-            <a href="#video"> Videos </a>
-          </p>
-
-        </section>
-
-        <section className="section-right">
-
-          <ProfileBox />
-
-          <BlogEntry />
-
-          <AboutBox />
-        </section>
+        </main>
 
       </main>
-
-    </main>
   );
 }
